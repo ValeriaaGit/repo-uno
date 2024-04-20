b5f80c3a443304649cc9155f8102a4d606d4d506
Valeria Serrano <167650618+ValeriaaGit@users.noreply.github.com>
2024-04-20 19:05:47 -0400

    Creating a testing cpp file called codigo_calis.cpp

---
+++ codigo_calis.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+  string name = "Paulina";
+
+  cout << "Este es un nombre prueba: " << name << endl;
+  return 0;
+}
